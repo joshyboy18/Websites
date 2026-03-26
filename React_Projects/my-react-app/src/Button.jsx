de0db1a01a55ea3846bac97d176c27593d8dcb68
PizzaTime16 <joshybosh9@gmail.com>
2026-03-26 11:12:53 -0500

    Added react project to repository

---
+++ React_Projects/my-react-app/src/Button.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react"
+
+
+function Button() {
+
+    return (
+        <>
+            <button className = "save" id = "save_button"> Save </button>
+        </>
+        
+    );
+}
+
+export default Button
